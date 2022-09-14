6907c4130860ebd0ca0e22e1850435e32aa8a0e3
SantoAntlab <santhosh.b@antlab.io>
2022-09-14 12:31:44 +0530

    php pages created

---
+++ common/sidebar.php
@@ -0,0 +1,85 @@
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
+        <ul class="nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="dashboard.html">
+              <i class="mdi mdi-view-dashboard menu-icon"></i>
+              <span class="menu-title">Dashboard</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+              <i class="menu-icon mdi mdi-file-document"></i>
+              <span class="menu-title">Inventory Analysis</span>
+              <i class="menu-arrow"></i> 
+            </a>
+            <div class="collapse" id="ui-basic">
+              <ul class="nav flex-column sub-menu">
+                <li class="nav-item"> <a class="nav-link" href="#">Buttons</a></li>
+                <li class="nav-item"> <a class="nav-link" href="#">Dropdowns</a></li>
+                <li class="nav-item"> <a class="nav-link" href="#">Typography</a></li>
+              </ul>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" data-bs-toggle="collapse" href="#ui-CRM" aria-expanded="false" aria-controls="ui-basic">
+              <i class="menu-icon mdi mdi-vector-combine"></i>
+              <span class="menu-title">CRM</span>
+              <i class="menu-arrow"></i> 
+            </a>
+            <div class="collapse" id="ui-CRM">
+              <ul class="nav flex-column sub-menu">
+                <li class="nav-item"> <a class="nav-link" href="crm-customer.php">Customer</a></li>
+                <li class="nav-item"> <a class="nav-link" href="#">Company</a></li>
+                <li class="nav-item"> <a class="nav-link" href="#">Quotes</a></li>
+                <li class="nav-item"> <a class="nav-link" href="#">Invoice</a></li>
+              </ul>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" data-bs-toggle="collapse" href="#ui-purchase" aria-expanded="false" aria-controls="ui-basic">
+              <i class="menu-icon mdi mdi-cart-outline"></i>
+              <span class="menu-title">Purchase</span>
+              <i class="menu-arrow"></i> 
+            </a>
+            <div class="collapse" id="ui-purchase">
+              <ul class="nav flex-column sub-menu">
+                <li class="nav-item"> <a class="nav-link" href="#">Vendors</a></li>
+                <li class="nav-item"> <a class="nav-link" href="#">Purchase Order</a></li>
+                <li class="nav-item"> <a class="nav-link" href="#">Payments</a></li>
+              </ul>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" data-bs-toggle="collapse" href="#ui-sales" aria-expanded="false" aria-controls="ui-basic">
+              <i class="menu-icon mdi mdi-chart-line"></i>
+              <span class="menu-title">Sales Analysis</span>
+              <i class="menu-arrow"></i> 
+            </a>
+            <div class="collapse" id="ui-sales">
+              <ul class="nav flex-column sub-menu">
+                <li class="nav-item"> <a class="nav-link" href="#">Out For Sale</a></li>
+                <li class="nav-item"> <a class="nav-link" href="#">Sales Tracker</a></li>
+              </ul>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <i class="mdi mdi-account-circle-outline menu-icon"></i>
+              <span class="menu-title">User Management</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <i class="mdi mdi-account-key menu-icon"></i>
+              <span class="menu-title">Roles & Permission</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <i class="mdi mdi-wifi menu-icon"></i>
+              <span class="menu-title">Feed</span>
+            </a>
+          </li>
+          
+                  </ul>
+      </nav>
